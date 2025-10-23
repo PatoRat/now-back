2e03c51baa8539b77e6591594e9d876fb0213163
Patricio <ratcliffepatricio321@gmail.com>
2025-10-23 01:11:41 -0300

    Tengo que adaptar el index.ts, al cual voy a llamar api.ts, para que funcione con el modelo que tenemos

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,36 @@
 import { PrismaClient, Prisma } from '@prisma/client'
+import { create } from 'domain'
 
 const prisma = new PrismaClient()
+
+const user: Prisma.UserCreateInput = {
+  email: "juan@gmail.com",
+  nombre: "Juan",
+  contrasenia: "Asd123123",
+  mis_eventos: {
+    create: [
+      {
+        nombre: "Afrika",
+        descripcion: "Hoy, noche del mes en Afrika Club, no te la pierdas\nJunin 1787, a partir de las 22:30",
+        imagenes: {
+          create: [
+            {
+
+              url: "https://lh3.googleusercontent.com/p/AF1QipOvl8JNluUE7VeTDIs_od4sW2ZY6C330W-KHBwy=s680-w680-h510-rw" 
+            },
+            { 
+              url: "https://lh3.googleusercontent.com/gps-cs-s/AC9h4noydtcHzXNvnQ0dJ33yWeUPA2N5bUXKWj_gnZb5vYLNMmB-XUxaWrvFzLUt6LTYDdQW2-tmsrL9w79QZOnURvDLHxRiDb2pNPYb9M63K14lK1_A_gZ968FbTwTcu4or_UAd1lMl=s680-w680-h510-rw"
+            }
+          ]
+        },
+        direccion: "calle 13",
+        fechaInicio: "10/10/2020",
+        fechaFin: "10/10/2020"
+      },
+    ]
+  },
+  favs: { create: [] }
+}
 
 // const userData: Prisma.UserCreateInput[] = [
 //   {
